b4805a97e022f657a02ffee7c68f3b9367984688
sg19112002 <sivaganeshsundaramoorthi@gmail.com>
2022-10-11 22:01:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import styles from './style';
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={'${styles.paddingX} ${styles.flexCenter}'}>
+      <div className={'${styles.boxWidth}'}>
+        navbar
+      </div>
+    </div>
+    <div className={'bg-primary ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        hero
+      </div>
+    </div>
+    <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        stats
+        footer
+      </div>
+    </div>
+  </div>
+  
+);
+
+export default App
