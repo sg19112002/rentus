2643c4fe0d9136f286aa3bcea53ecfa6e4ba209e
Roshan-Baskaran <roshanbaskaran2002@gmail.com>
2022-10-11 22:52:47 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './style';
 //First comment
+//Second commene
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={'${styles.paddingX} ${styles.flexCenter}'}>
