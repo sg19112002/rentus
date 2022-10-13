3be48082dfa90086ce5368d9afcfba8c9fe220ca
sg19112002 <sivaganeshsundaramoorthi@gmail.com>
2022-10-13 20:34:52 +0530

    logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {close,logo,menu} from '../assets';
+import {navLinks} from '../constants';
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="w-full flex py-6 justify-between items-centre navbar">
+      <img src={logo} alt="ship" className="w-[174px] h-[50px] pr-0" />
+    </nav>
   )
 }
 
