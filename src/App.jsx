a4e54aeca5ae681223d04ca8c6af62b778b2417e
sg19112002 <sivaganeshsundaramoorthi@gmail.com>
2022-10-14 18:03:39 +0530

    CART

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary text-white ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
     
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`bg-primary text-white ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Footer />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import {close,logo,menu} from '../assets';
-import {navLinks} from '../constants';
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+import { Link } from "react-router-dom";
 
 const navbar = () => {
   return (
-    <nav className="w-full flex py-1 justify-between items-centre navbar">
-      <img src={logo} alt="ship" className="w-[140px] h-[50px] " />
+    <nav className="w-full flex py-1 space-x-3 items-center justify-between navbar ">
+      <img src={logo} alt="logo" className="w-[140px] h-[50px] " />
+      <div className="flex flex-row p-5">
+        <Link to="/" className="bg-black text-white hidden px-2 sm:flex  ">
+          CART
+        </Link>
+        <Link to="/" className="bg-black text-white hidden px-2 sm:flex  ">
+          LOGIN
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default navbar;
