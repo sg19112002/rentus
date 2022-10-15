4a496dde5ba24a4b6f3d4e2180714e4d078b97b2
sg19112002 <sivaganeshsundaramoorthi@gmail.com>
2022-10-15 11:00:02 +0530

    new logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 const navbar = () => {
   return (
     <nav className="w-full flex  items-center justify-between navbar ">
-      <img src={logo} alt="logo" className="w-[140px] h-[50px] " />
+      <img src={logo} alt="logo" className="w-[120px] h-[70px] " />
       <div className="bg-white rounded-full flex items-center px-2 w-{400px} sm:w-[500px] lg:w-[400] ">
         <AiOutlineSearch size={25} />
         <input
@@ -16,7 +16,7 @@ const navbar = () => {
           placeholder="Search the Product here..........."
         />
       </div>
-      <div className="flex flex-row p">
+      <div className="flex flex-row space-x-1.5">
         <Link
           to="/"
           className="bg-violet-400 text-white px-2 sm:flex  rounded-full inline-block hover:bg-green-700"
@@ -37,8 +37,22 @@ const navbar = () => {
           </svg>
           cart(0)
         </Link>
-        <Link to="/" className=" hidden px-4 sm:flex ">
-          LOGIN
+        <Link to="/" className=" bg-black text-white hidden px-3 sm:flex rounded-full ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          Signin
         </Link>
       </div>
     </nav>
