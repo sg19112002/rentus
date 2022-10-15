bdf15471028afd56a42a568f672ffbef16029a66
sg19112002 <sivaganeshsundaramoorthi@gmail.com>
2022-10-16 01:17:03 +0530

    hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Typed from "react-typed";
 
 const hero = () => {
   return (
-    <div>hero</div>
-  )
-}
+    <div>
+      <Typed strings={["still buying product","time to change","start renting the product"]} typeSpeed={40} />
+    </div>
+  );
+};
 
 export default hero;
