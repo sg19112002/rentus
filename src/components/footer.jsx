cb1c3b811d3a4a5f0884f86c39c243aa5b57d074
Roshan-Baskaran <roshanbaskaran2002@gmail.com>
2022-10-16 20:21:58 +0530

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="max-w-[1240px] mx-auto py-10 px-3 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">rentus</h1>
+        <div className="flex justify-between md:w-[75%] my-6">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
+        </div>
+      </div>
+      <div className="lg:col-span-2 flex justify-between mt-6 w-full pr-10 ">
+        <ul className="flex space-x-8 text-black">
+          <li className="py-2 text-sm ">About us</li>
+          <li className="py-2 text-sm">Terms and Conditions</li>
+          <li className="py-2 text-sm">Commerce</li>
+          <li className="py-2 text-sm">Support</li>
+          <li className="py-2 text-sm">Copyright policy</li>
+
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
